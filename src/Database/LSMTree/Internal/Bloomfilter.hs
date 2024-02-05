ff45450f1e4709b35fd501eb6c2f9e5509ca3f0e
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-02-05 12:53:52 +0200

    WIP

--- src/Database/LSMTree/Internal/Bloomfilter.hs
+++ src/Database/LSMTree/Internal/Bloomfilter.hs
@@ -2,6 +2,9 @@
 module Database.LSMTree.Internal.Bloomfilter where 
 
 import Data.Word (Word32)
+import System.IO (withFile, IOMode (..))
+import qualified Data.ByteString.Builder as B
+import qualified Data.ByteString.Lazy as LBS
 
 import qualified Data.BloomFilter as BF
 
@@ -12,16 +15,24 @@ data BloomFilterHeader = BloomFilterHeader
     , bfhEndianess :: !Word32
     }
 
-writeBloomFilter :: FilePath -> BF.Bloom a -> IO ()
-writeBloomFilter fp bf = do
-    return ()
+bloomFilterVersion :: Word32
+bloomFilterVersion = 0
+
+toBuilder :: BF.Bloom a -> B.Builder
+toBuilder bf =
+    BF.word32BE (bfhVersion header) <>
+    BF.word32BE (bfhHashCount header) <>
+    BF.word32BE (bfhBitSize header) <>
+    BF.word32BE (bfhEndianess)
   where
     !header = BloomFilterHeader
-        { bfhVersion = 0
-        , bfhHashCount = fromIntegral 0 -- TODO
-        , bfhBitSize = fromIntegral (BF.length bf)
+        { bfhVersion   = bloomFilterVersion
+        , bfhHashCount = fromIntegral (BF.hashesN bf)
+        , bfhBitSize   = fromIntegral (BF.length bf)
         , bfhEndianess = 0 -- TODO
         }
+
+writeBloomFilter :: FilePath -> BF.Bloom a -> IO ()
 
 readBloomFilter :: IO (BF.Bloom a)
 readBloomFilter = fail "TODO"
